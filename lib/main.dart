b451bd2bc2ef92c94db245dfba41b29eeaf9f727
kemcz <kleirbonitz@gmail.com>
2022-02-23 14:19:25 +0800

    Magic Ball Flutter Project

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightGreen,
+        appBar: AppBar(
+          title: const Center(
+            child: Text('Ask me anything'),
+          ),
+          backgroundColor: Colors.lightGreen[300],
+        ),
+        body: Ball(),
+      ),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
